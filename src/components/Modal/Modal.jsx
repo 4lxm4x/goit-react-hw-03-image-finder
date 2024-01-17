b71d5c2aa369ac93fc3f7a549698b199bcc7fa09
Dan6kav <dan6kav@gmail.com>
2024-01-17 17:40:15 +0200

    second try to push

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,20 +1,6 @@
 import { Component } from 'react';
 import { createPortal } from 'react-dom';
 import '../../styles.css';
-// import { Component } from 'react';
-
-// export default function Modal(largeImage, onOverlayClick) {
-//   const modalRootEl = document.getElementById('modal-root');
-
-//   return createPortal(
-//     <div className="Overlay" onClick={onOverlayClick}>
-//       <div className="Modal">
-//         <img src={largeImage.largeImage} className="modalImage" />
-//       </div>
-//     </div>,
-//     modalRootEl
-//   );
-// }
 
 class Modal extends Component {
   modalRootEl = document.getElementById('modal-root');
